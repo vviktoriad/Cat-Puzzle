642c57115ea6c2bebf23f1d6ed2e59def183ced1
vviktoriad <121107681+vviktoriad@users.noreply.github.com>
2024-07-25 15:18:51 +0200

    changes

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const image = document.getElementsById('puzzle-image')
-const canvas = document.getElementsById('puzzle-canvas')
+const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
+const image = document.getElementById('puzzle-image')
+image.onload = cutImage;
+
+function cutImage() {
+    let imagePieces = [];
+    for (let x = 0; x < 3; ++x) {
+        for (let y = 0; y < 3; ++y) {
+            if (x != 2 || y != 2) {
+                ctx.drawImage(image, x * 100, y * 100, 100, 100, x * 100, y * 100, 100, 100)
+            }
+
+        }
+    }
+    let imageElement = document.getElementById('image-element')
+    imageElement.src = imagePieces[0]
+}
+
 
