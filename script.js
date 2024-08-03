3894458c37d12d031a276547ae354eea5ee33714
Wiktoria Dawiec <wdawiec@student.agh.edu.pl>
2024-08-03 23:38:03 +0200

    changes

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const image = document.getElementById('puzzle-image')
 image.onload = cutImage;
 
 function cutImage() {
-    let imagePieces = [];
-    for (let x = 0; x < 3; ++x) {
-        for (let y = 0; y < 3; ++y) {
+    let number = 1;
+    for (let y = 0; y < 3; ++y) {
+        for (let x = 0; x < 3; ++x) {
             if (x != 2 || y != 2) {
                 ctx.drawImage(image, x * 100, y * 100, 100, 100, x * 100, y * 100, 100, 100)
+                ctx.fillStyle = 'white';
+                ctx.fillText(number, x * 100 + 10, y * 100 + 10)
+                number++;
             }
-
         }
     }
-    let imageElement = document.getElementById('image-element')
-    imageElement.src = imagePieces[0]
 }
+
 
 
