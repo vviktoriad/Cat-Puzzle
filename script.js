1af75c29c8ee31e11d7a1a0e5e4c6c2a5057a2f2
Wiktoria Dawiec <wdawiec@student.agh.edu.pl>
2024-10-24 17:40:44 +0200

    changes

--- script.js
+++ script.js
@@ -3,19 +3,45 @@ const ctx = canvas.getContext('2d')
 const image = document.getElementById('puzzle-image')
 image.onload = cutImage;
 
+let puzzles = []
+
 function cutImage() {
     let number = 1;
     for (let y = 0; y < 3; ++y) {
         for (let x = 0; x < 3; ++x) {
-            if (x != 2 || y != 2) {
-                ctx.drawImage(image, x * 100, y * 100, 100, 100, x * 100, y * 100, 100, 100)
-                ctx.fillStyle = 'white';
-                ctx.fillText(number, x * 100 + 10, y * 100 + 10)
+            if (x !=2 || y != 2) {
+                puzzles.push({
+                    x: x * 100,
+                    y: y * 100,
+                    width: 100,
+                    height: 100,
+                    number: number
+                });
                 number++;
             }
         }
     }
+    drawPuzzles();
 }
+
+
+function drawPuzzles() {
+    puzzles.forEach(puzzle => {
+        
+        ctx.drawImage(image, puzzle.x, puzzle.y, puzzle.width, puzzle.height, puzzle.x, puzzle.y, puzzle.width, puzzle.height)
+
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.strokeRect(puzzle.x, puzzle.y, puzzle.width, puzzle.height);
+
+        ctx.fillStyle = 'white';
+        ctx.fillText(puzzle.number, puzzle.x + 10, puzzle.y + 10)
+            
+        });
+    }
+
+
+
 
 
 
